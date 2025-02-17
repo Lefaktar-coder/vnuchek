fd3356e6b447a26ee7efc19a38f47bdd63f12a9d
lefaktar <lefaktar@yandex.ru>
2025-02-17 18:21:10 +0300

    add output

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	output: 'export',
-	/* config options here */
+	trailingSlash: true,
+	i18n: undefined, // Отключить, если не используешь
+	rewrites: undefined,
+	redirects: undefined,
 }
 
 export default nextConfig
