ed7e739b9c3e6220507e93e2c41ecb38741434a8
lefaktar <lefaktar@yandex.ru>
2025-02-18 10:27:41 +0300

    add output && images rules

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	output: 'export',
+	reactStrictMode: false,
+	swcMinify: true,
 	images: {
+		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: 'http',
