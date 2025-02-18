1a5d417119f4b3b392db0482e68691aa6857ea2a
lefaktar <lefaktar@yandex.ru>
2025-02-18 10:18:35 +0300

    fix output

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	reactStrictMode: false,
-	swcMinify: true,
 	images: {
-		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
 	},
 }
 
