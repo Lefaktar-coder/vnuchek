cdd7e5eb9ecbc1eaf7681a6f79f398607e847ac3
lefaktar <lefaktar@yandex.ru>
2025-02-19 09:55:08 +0300

    add favicon

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	reactStrictMode: false,
-	swcMinify: true,
-	images: {
-		unoptimized: true,
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: '**',
-			},
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-	},
+
 }
 
 export default nextConfig
